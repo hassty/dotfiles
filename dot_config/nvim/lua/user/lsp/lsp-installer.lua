cf7b68ea906aeac2adf8c5355328f3fe4c1ee9f1
-hasty- <tikhomirovtimofei@gmail.com>
2022-02-23 22:44:53 +0300

    Temporarily disable lsp autoinstall

--- dot_config/nvim/lua/user/lsp/lsp-installer.lua
+++ dot_config/nvim/lua/user/lsp/lsp-installer.lua
@@ -29,13 +29,13 @@ local servers = {
 	"sqls",
 }
 
-for _, name in pairs(servers) do
-	local server_is_found, server = lsp_installer.get_server(name)
-	if server_is_found and not server:is_installed() then
-		print("Installing " .. name)
-		server:install()
-	end
-end
+-- for _, name in pairs(servers) do
+-- 	local server_is_found, server = lsp_installer.get_server(name)
+-- 	if server_is_found and not server:is_installed() then
+-- 		print("Installing " .. name)
+-- 		server:install()
+-- 	end
+-- end
 
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
@@ -48,6 +48,16 @@ lsp_installer.on_server_ready(function(server)
 	if server.name == "sumneko_lua" then
 		local sumneko_opts = require("user.lsp.settings.sumneko_lua")
 		opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+	end
+
+	if server.name == "tsserver" then
+		local tsserver_opts = require("user.lsp.settings.tsserver")
+		opts = vim.tbl_deep_extend("force", tsserver_opts, opts)
+	end
+
+	if server.name == "clangd" then
+		local clangd_opts = require("user.lsp.settings.clangd")
+		opts = vim.tbl_deep_extend("force", clangd_opts, opts)
 	end
 
 	-- This setup() function is exactly the same as lspconfig's setup function.
