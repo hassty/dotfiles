821fc90a6ce7deeed0ba98268211be11609ad0af
-hasty- <tikhomirovtimofei@gmail.com>
2022-02-27 16:31:21 +0300

    Nvim indentline don't show current context start

--- dot_config/nvim/lua/user/indentline.lua
+++ dot_config/nvim/lua/user/indentline.lua
@@ -54,5 +54,5 @@ indent_blankline.setup({
 	show_end_of_line = true,
 	space_char_blankline = " ",
 	show_current_context = true,
-	show_current_context_start = true,
+	show_current_context_start = false,
 })
