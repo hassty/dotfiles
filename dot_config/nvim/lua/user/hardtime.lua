dc247dc4270769b3d3107e24449147322e93caba
-hasty- <tikhomirovtimofei@gmail.com>
2022-03-26 14:06:19 +0300

    Add dbui to hardtime ignore list

--- dot_config/nvim/lua/user/hardtime.lua
+++ dot_config/nvim/lua/user/hardtime.lua
@@ -3,5 +3,5 @@ let g:hardtime_default_on = 1
 let g:hardtime_allow_different_key = 1
 let g:hardtime_maxcount = 2
 let g:list_of_normal_keys = ["h", "j", "k", "l", "<C-n>", "<C-p>", "w", "b", "e", "W", "B", "E"]
-let g:hardtime_ignore_buffer_patterns = ["NvimTree"]
+let g:hardtime_ignore_buffer_patterns = ["NvimTree", "dbui"]
 ]])
