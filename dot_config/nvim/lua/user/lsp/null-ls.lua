cdaee04a3762d5000b026b0c1cb71793704ba384
-hasty- <tikhomirovtimofei@gmail.com>
2022-04-23 23:18:54 +0300

    Get rust edition from Cargo.toml

--- dot_config/nvim/lua/user/lsp/null-ls.lua
+++ dot_config/nvim/lua/user/lsp/null-ls.lua
@@ -26,7 +26,23 @@ null_ls.setup({
 				"html",
 			},
 		}),
-		formatting.rustfmt,
+		formatting.rustfmt.with({
+			extra_args = function(params)
+				local Path = require("plenary.path")
+				local cargo_toml = Path:new(params.root .. "/" .. "Cargo.toml")
+
+				if cargo_toml:exists() and cargo_toml:is_file() then
+					for _, line in ipairs(cargo_toml:readlines()) do
+						local edition = line:match([[^edition%s*=%s*%"(%d+)%"]])
+						if edition then
+							return { "--edition=" .. edition }
+						end
+					end
+				end
+				-- default edition when we don't find `Cargo.toml` or the `edition` in it.
+				return { "--edition=2021" }
+			end,
+		}),
 		formatting.shfmt,
 		formatting.sqlformat,
 		formatting.stylua,
