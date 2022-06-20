1d9bad25ca092a8d714637331241d86d0785de20
-hasty- <tikhomirovtimofei@gmail.com>
2022-06-20 15:08:14 +0300

    Rewrite dbout.vim in lua

---
+++ dot_config/nvim/ftplugin/dbout.lua
@@ -0,0 +1,20 @@
+local augroup = vim.api.nvim_create_augroup("dbui", { clear = true })
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	group = augroup,
+	pattern = "*.dbout",
+	command = "set filetype=dbout",
+})
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+	group = augroup,
+	pattern = "*.dbout",
+	callback = function()
+		vim.cmd("call db_ui#save_dbout(expand('<afile>'))")
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	group = augroup,
+	pattern = "*.dbout",
+	command = "",
+})
--- dot_config/nvim/ftplugin/dbout.vim
+++
@@ -1,6 +0,0 @@
-augroup dbui
-  autocmd!
-  autocmd BufRead,BufNewFile *.dbout set filetype=dbout
-  autocmd BufReadPost *.dbout nested call db_ui#save_dbout(expand('<afile>'))
-  autocmd FileType dbout,dbui autocmd BufEnter,WinEnter <buffer> stopinsert
-augroup END
