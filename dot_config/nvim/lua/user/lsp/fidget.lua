9ab47e756c1c8932bd41dcc1a0135eb7ce99b5b1
-hasty- <tikhomirovtimofei@gmail.com>
2022-06-20 17:29:36 +0300

    Change nvim lsp fidget style

--- dot_config/nvim/lua/user/lsp/fidget.lua
+++ dot_config/nvim/lua/user/lsp/fidget.lua
@@ -5,8 +5,8 @@ end
 
 fidget.setup({
 	text = {
-		spinner = "dots", -- animation shown when tasks are ongoing
-		done = "✔", -- character shown when all tasks are complete
+		spinner = "meter", -- animation shown when tasks are ongoing
+		done = "✓", -- character shown when all tasks are complete
 		commenced = "Started", -- message shown when task starts
 		completed = "Completed", -- message shown when task completes
 	},
