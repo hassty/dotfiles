32178f74371bdeef01689f9003c17b987818d957
-hasty- <tikhomirovtimofei@gmail.com>
2022-06-27 21:55:06 +0300

    Add ytfzf config

---
+++ dot_config/ytfzf/executable_conf.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+show_thumbnails=1
+fzf_preview_side=right
+print_link_shortcut=ctrl-o
+enable_hist=0
+enable_search_hist=0
+# log_level=1
+log_level=2
+
+video_player() {
+	#this function should not be set as the url_handler as it is part of multimedia_player
+	command_exists "mpv" || die 3 "mpv is not installed\n"
+	DRI_PRIME=1 mpv "$@"
+}
