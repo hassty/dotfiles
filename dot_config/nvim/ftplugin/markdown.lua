1fe5ec36242d59e5f6177b6993b12a6b4b847c41
-hasty- <tikhomirovtimofei@gmail.com>
2022-12-16 20:37:51 +0300

    Disable nvim markdown autoclose

--- dot_config/nvim/ftplugin/markdown.lua
+++ dot_config/nvim/ftplugin/markdown.lua
@@ -4,3 +4,4 @@ vim.g.vim_markdown_strikethrough = 1
 vim.g.vim_markdown_auto_insert_bullets = 0
 vim.g.vim_markdown_new_list_item_indent = 0
 vim.g.vim_markdown_folding_disabled = 1
+vim.g.mkdp_auto_close = 0
