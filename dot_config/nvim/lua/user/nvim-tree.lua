91579590ef10b3a2dfb7a44ce3a8447ab60fd295
-hasty- <tikhomirovtimofei@gmail.com>
2023-04-16 11:49:02 +0300

    Remove nvim-tree deprecated options

--- dot_config/nvim/lua/user/nvim-tree.lua
+++ dot_config/nvim/lua/user/nvim-tree.lua
@@ -17,9 +17,6 @@ nvim_tree.setup({
 	hijack_cursor = true,
 	hijack_netrw = true,
 	hijack_unnamed_buffer_when_opening = false,
-	ignore_buffer_on_setup = false,
-	open_on_setup = false,
-	open_on_setup_file = false,
 	open_on_tab = false,
 	sort_by = "name",
 	update_cwd = true,
@@ -87,12 +84,6 @@ nvim_tree.setup({
 		enable = false,
 		update_cwd = true,
 		ignore_list = {},
-	},
-	ignore_ft_on_setup = {
-		"startify",
-		"dashboard",
-		"alpha",
-		"help",
 	},
 	system_open = {
 		cmd = "",
